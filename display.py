cb8d2d1daedcae485b6218057c0e2b9435277bb3
Karo <karolina.stachowiak0@gmail.com>
2023-03-17 20:48:57 +0100

    Introduce Displayer class

--- display.py
+++ display.py
@@ -4,88 +4,90 @@ from circle import Circle
 from regular_pentagon import RegularPentagon
 from rhombus import Rhombus
 
-def display_menu():
-    user_input = input("Learn Geometry.\n  What do you want to do?\n  (1) Add new shape\n  (2) Show all shapes\n  (3) Show shape with the largest perimeter\n  (4) Show shape with the largest area\n  (5) Show formulas\n  (0) Exit program\n")
-    return user_input
+class Displayer():
 
-def inform_the_selection_is_invalid():
-    print("\nInvalid selection. Please select a number 0-5\n")
+    def display_menu(self):
+        user_input = input("Learn Geometry.\n  What do you want to do?\n  (1) Add new shape\n  (2) Show all shapes\n  (3) Show shape with the largest perimeter\n  (4) Show shape with the largest area\n  (5) Show formulas\n  (0) Exit program\n")
+        return user_input
 
-def choose_shape_type_to_add():
-    shape_type = input("What shape would you like to add? ")
-    if shape_type == "circle":
-        r = float(input("Please enter the radius of the circle: "))
-        shape = Circle(r)
+    def inform_the_selection_is_invalid(self):
+        print("\nInvalid selection. Please select a number 0-5\n")
 
-    elif shape_type == "triangle":
-        a = float(input("Please enter the length of side a: "))
-        b = float(input("Please enter the length of side b: "))
-        c = float(input("Please enter the length of side c: "))
-        shape = Triangle(a, b, c)
+    def choose_shape_type_to_add(self):
+        shape_type = input("What shape would you like to add? ")
+        if shape_type == "circle":
+            r = float(input("Please enter the radius of the circle: "))
+            shape = Circle(r)
 
-    elif shape_type == "equilateral triangle":
-        a = float(input("Please enter the side length of the triangle: "))
-        shape = EquilateralTriangle(a)
+        elif shape_type == "triangle":
+            a = float(input("Please enter the length of side a: "))
+            b = float(input("Please enter the length of side b: "))
+            c = float(input("Please enter the length of side c: "))
+            shape = Triangle(a, b, c)
 
-    elif shape_type == "rectangle":
-        a = float(input("Please enter the width of the rectangle: "))
-        b = float(input("Please enter the height of the rectangle: "))
-        shape = Rectangle(a,b)
+        elif shape_type == "equilateral triangle":
+            a = float(input("Please enter the side length of the triangle: "))
+            shape = EquilateralTriangle(a)
 
-    elif shape_type == "square":
-        a = float(input("Please enter the side length of the square: "))
-        shape = Square(a)
+        elif shape_type == "rectangle":
+            a = float(input("Please enter the width of the rectangle: "))
+            b = float(input("Please enter the height of the rectangle: "))
+            shape = Rectangle(a,b)
+
+        elif shape_type == "square":
+            a = float(input("Please enter the side length of the square: "))
+            shape = Square(a)
+        
+        elif shape_type == "regular pentagon":
+            a = float(input("Please enter the side length of the pentagon: "))
+            shape = RegularPentagon(a)
+
+        elif shape_type == "rhombus":
+            a = float(input("Please enter the side length of the rhombus: "))
+            d1 = float(input("Please enter the first diagonal length: "))
+            d2 = float(input("Please enter the second diagonal length: "))
+            shape = Rhombus(a,d1,d2)
+
+        else:
+            print("Invalid shape name")
+            return self.choose_shape_type_to_add()
+        print(f"{shape} has been added to the list")
+        return shape
+        
     
-    elif shape_type == "regular pentagon":
-        a = float(input("Please enter the side length of the pentagon: "))
-        shape = RegularPentagon(a)
+    def show_formulas(self):
+        shape = input("Formula of which shape would you like to see? ")
+        if shape == "circle":
+            circle = Circle(1)
+            self.print_perimeter_and_area_formula(circle)
 
-    elif shape_type == "rhombus":
-        a = float(input("Please enter the side length of the rhombus: "))
-        d1 = float(input("Please enter the first diagonal length: "))
-        d2 = float(input("Please enter the second diagonal length: "))
-        shape = Rhombus(a,d1,d2)
+        elif shape == "triangle":
+            triangle = Triangle(1,2,3)
+            self.print_perimeter_and_area_formula(triangle)
 
-    else:
-        print("Invalid shape name")
-        return choose_shape_type_to_add()
-    print(f"{shape} has been added to the list")
-    return shape
-    
- 
-def show_formulas():
-    shape = input("Formula of which shape would you like to see? ")
-    if shape == "circle":
-        circle = Circle(1)
-        print_perimeter_and_area_formula(circle)
+        elif shape == "equilateral triangle":
+            equilateral_triangle = EquilateralTriangle(1)
+            self.print_perimeter_and_area_formula(equilateral_triangle)
 
-    elif shape == "triangle":
-        triangle = Triangle(1,2,3)
-        print_perimeter_and_area_formula(triangle)
+        elif shape == "rectangle":
+            rectangle = Rectangle(1,2)
+            self.print_perimeter_and_area_formula(rectangle)
 
-    elif shape == "equilateral triangle":
-        equilateral_triangle = EquilateralTriangle(1)
-        print_perimeter_and_area_formula(equilateral_triangle)
+        elif shape == "square":
+            square = Square(1)
+            self.print_perimeter_and_area_formula(square)
 
-    elif shape == "rectangle":
-        rectangle = Rectangle(1,2)
-        print_perimeter_and_area_formula(rectangle)
+        elif shape == "regular pentagon":
+            regular_pentagon = RegularPentagon(1)
+            self.print_perimeter_and_area_formula(regular_pentagon)
 
-    elif shape == "square":
-        square = Square(1)
-        print_perimeter_and_area_formula(square)
+        elif shape == "rhombus":
+            rhombus = Rhombus(1,2,3)
+            self.print_perimeter_and_area_formula(rhombus)
 
-    elif shape == "regular pentagon":
-        regular_pentagon = RegularPentagon(1)
-        print_perimeter_and_area_formula(regular_pentagon)
+        else:
+            print("Invalid shape name")
+            return self.show_formulas()
 
-    elif shape == "rhombus":
-        rhombus = Rhombus(1,2,3)
-        print_perimeter_and_area_formula(rhombus)
-
-    else:
-        print("Invalid shape name")
-        return show_formulas()
-
-def print_perimeter_and_area_formula(shape):
-    print ((f"perimeter formula: {shape.get_perimeter_formula()}\narea formula: {shape.get_area_formula()}"))
+    def print_perimeter_and_area_formula(self,shape):
+        print ((f"perimeter formula: {shape.get_perimeter_formula()}\narea formula: {shape.get_area_formula()}"))
--- program.py
+++ program.py
@@ -1,23 +1,24 @@
-from display import display_menu, inform_the_selection_is_invalid, choose_shape_type_to_add, show_formulas
+from display import Displayer
 from shape_list import ShapeList
 
 class Program:
     def __init__(self):
         self.shape_list = ShapeList()
-        
+        self.displayer = Displayer()
+
     def run(self):
-        user_input = display_menu()
+        user_input = self.displayer.display_menu()
         if not user_input.isdigit():
-            inform_the_selection_is_invalid()
+            self.displayer.inform_the_selection_is_invalid()
             return self.run()
         user_input = int(user_input)
         if user_input not in range(6):
-            inform_the_selection_is_invalid()
+            self.displayer.inform_the_selection_is_invalid()
             return self.run() 
         if user_input == 0:
             quit()
         if user_input == 1:
-            shape = choose_shape_type_to_add()
+            shape = self.displayer.choose_shape_type_to_add()
             self.shape_list.add_shape(shape)
             return self.run() 
         if user_input == 2:
@@ -30,10 +31,10 @@ class Program:
             self.shape_list.get_largest_shape_by_area()
             return self.run() 
         if user_input == 5:   
-            show_formulas()
+            self.displayer.show_formulas()
             return self.run() 
         else:
-            inform_the_selection_is_invalid()
+            self.displayer.inform_the_selection_is_invalid()
             return self.run() 
 
 geometry_program = Program()
